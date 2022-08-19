f8bb94995e0fa84c491168f416f628e67a26ec7d
Pavel Perestoronin <eigenein@gmail.com>
2022-08-19 12:24:37 +0200

    💚 Enable additional Clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 //! Binomial proportion confidence intervals.
 
+#![warn(
+    clippy::all,
+    clippy::missing_const_for_fn,
+    clippy::trivially_copy_pass_by_ref,
+    clippy::map_unwrap_or,
+    clippy::explicit_into_iter_loop,
+    clippy::unused_self,
+    clippy::needless_pass_by_value
+)]
+
 pub mod error;
 pub mod interval;
 pub mod math;
